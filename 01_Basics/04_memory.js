7ef6318fa01a33564435fef3f4f4f23fd66ff4c4
Jay Padasala <padasalajay1781@gmail.com>
2024-03-08 02:16:12 +0530

    end Task

---
+++ 01_Basics/04_memory.js
@@ -0,0 +1,30 @@
+// Stack ( primitive type), Heap Memory ( Non- Premitive)
+
+/*
+
+
+ */
+//
+// let Myname = "Padasala";
+// let SecodName = Myname;
+// SecodName = "Jaykumar";
+
+// // console.log(Myname);
+// // console.log(SecodName);
+
+// // Heap
+// let Userone = {
+//   name: "Meet",
+//   age: "18",
+// };
+
+// let UserTwo = Userone;
+
+// UserTwo.name = "jay";
+
+// console.log(Userone);
+// console.log(UserTwo);
+
+const ganename = new String("Padasala");
+
+console.log(ganename); 
